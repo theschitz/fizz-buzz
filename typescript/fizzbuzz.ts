04b3d64b0c334d542c006b567e936974eca046d5
theschitz <theodore@theschitz.se>
2020-10-28 22:09:24 +0100

    fizzbuzz in typescript

---
+++ typescript/fizzbuzz.ts
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 100; i++) {
+    let output = "";
+    if (i % 3 == 0) { output += "Fizz"; }
+    if (i % 5 == 0) { output += "Buzz"; }
+    if (output === "") { output = i.toString(); }
+
+    console.log(output);
+}
