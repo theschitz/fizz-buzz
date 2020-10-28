db7e9194ed6651ea71b821c2e601d8f532dd3a8e
theschitz <theodore@theschitz.se>
2020-10-28 23:20:16 +0100

    add fizzbuzz c++

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fizz-buzz
 Well, yeah, FizzBuzz
+
+* bash
+* c
+* c++
+* c#
+* java
+* javascript
+* php
+* python
+* typescript
---
+++ cpp/fizzbuzz.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i <= 100; i++)
+    {
+        char output[8] = "";
+        if (i % 3 == 0)
+        {
+           strcat(output, "Fizz");
+        }
+        if (i % 5 == 0)
+        {
+            strcat(output, "Buzz");
+        }
+        if(output[0] == '\0') {
+            sprintf(output, "%d", i);
+        }
+
+        printf("%s\n",output);
+    }
+}
