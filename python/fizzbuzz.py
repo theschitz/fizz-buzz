d8cc841921fa564b19ad4ec71e3278ddc5892f1a
theschitz <theodore@theschitz.se>
2020-10-28 17:08:50 +0100

    python fizzbuzz

---
+++ python/fizzbuzz.py
@@ -0,0 +1,9 @@
+for i in range(101):
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if not output:
+        output = str(i)
+    print(output)
